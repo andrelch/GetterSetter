b25f42457940ea58eb960b345f20ca29952736f9
andrelch <andreleung88@gmail.com>
2022-09-26 10:00:18 -0400

    initial push

--- src/Location.java
+++ src/Location.java
@@ -1,19 +1,24 @@
 public class Location {
+    //private variables seen only in this class
     private int posX = 15;
     private int posY = 30;
 
+    //getter method posX
     public int getPosX(){
         return posX;
     }
 
+    //setter method posX
     public void setPosX(int setPosX){
         posX = setPosX;
     }
 
+    //getter method posY
     public int getPosY(){
         return posY;
     }
 
+    //setter method posY
     public void setPosY(int setPosY){
         posY = setPosY;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        //original coordinates
         Location location = new Location();
+        //print original coordinates
         System.out.println("Coordinates: " + location.getPosX() + ", " + location.getPosY());
+
+        //sets new coordinates already predefined
         location.setPosX(60);
         location.setPosY(120);
+        //prints new coordinates
         System.out.println("New Coordinates: " + location.getPosX() + ", " + location.getPosY());
+
+        //sets newer coordinates through user input
+        //import scanner class to read user input
+        Scanner input = new Scanner(System.in);
+        int inputPosX;
+        int inputPosY;
+
+        //setting posX
+        System.out.println("Enter newer x-coordinate:");
+        //parses input and turns string into integer
+        inputPosX = Integer.parseInt(input.nextLine());
+        location.setPosX(inputPosX);
+
+        //setting posY
+        System.out.println("Enter newer y-coordinate:");
+        //parses input and turns string into integer
+        inputPosY = Integer.parseInt(input.nextLine());
+        location.setPosY(inputPosY);
+
+        //prints newer coordinates
+        System.out.println("Newer Coordinates: " + location.getPosX() + ", " + location.getPosY());
     }
 }
